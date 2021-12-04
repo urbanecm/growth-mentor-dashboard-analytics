a2a960f461ea10fd88371cbdae73c40219338642
Martin Urbanec <martin.urbanec@wikimedia.cz>
2021-12-04 21:27:02 +0000

    Initial commit

---
+++ update_superset_mentor_dashboard.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+cd '/home/urbanecm/Documents/Growth/Mentor dashboard/analytics'
+
+source conda-activate-stacked 2021-04-07T23.21.00_urbanecm
+export PATH=/usr/local/bin:$PATH
+
+# web access is needed, as data about mentor counts are downloaded from the wikis
+export http_proxy=http://webproxy.eqiad.wmnet:8080
+export https_proxy=http://webproxy.eqiad.wmnet:8080
+
+jupyter nbconvert --to html --execute 'mentor-dashboard-analytics.ipynb'
+
+hdfs dfs -chmod -R o+rx /user/hive/warehouse/urbanecm_growth.db
